e53a3a2b3b0104c8da388be8c09f75d435ae0772
ppch <3363934824@qq.com>
2024-07-07 20:51:37 +0800

    实现基础聊天界面

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Register r;
-    r.show();
+    // Register r;
+    // r.show();
     MainWindow w;
 
-    if (r.exec() ==QDialog::Accepted){
-        w.show();
+    // if (r.exec() ==QDialog::Accepted){
+    //     w.show();
 
-        return a.exec();
-    }
-    // w.show();
+    //     return a.exec();
+    // }
+    w.show();
     return a.exec();
 }
